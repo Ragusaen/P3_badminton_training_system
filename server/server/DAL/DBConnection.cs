cba5fada61f4c5e284265d6118eac31e294a1240
Slorup <nicklas.slorup@yahoo.dk>
2019-10-07 11:34:50 +0200

    DBConn + ConnString

---
+++ server/server/DAL/DBConnection.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server.DAL
+{
+    class DBConnection
+    {
+        private string connString;
+
+        public DBConnection()
+        {
+            connString = ConfigurationManager.ConnectionStrings["DBConnString"].ConnectionString;
+        }
+
+        /// <summary>
+        /// Executes a select query and returns a DataTable with the results.
+        /// </summary>
+        /// <param name="query"></param>
+        /// <param name="sqlParameters"></param>
+        public DataTable ExecuteSelectQuery(string query, SqlParameter[] sqlParameters)
+        {
+            DataTable dt = new DataTable();
+            dt = null;
+            DataSet ds = new DataSet();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddRange(sqlParameters);
+                try
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    adapter.Fill(ds);
+                    dt = ds.Tables[0];
+                }catch(SqlException e)
+                {
+                    Console.WriteLine("Error ExecuteSelectQuery - " + e.Message);
+                }
+            }
+            return dt;
+        }
+
+        /// <summary>
+        /// Executes an insert/update/delete query and returns true if successful.
+        /// </summary>
+        /// <param name="query"></param>
+        /// <param name="sqlParameters"></param>
+        /// <returns></returns>
+        public bool ExecuteInsertUpdateDeleteQuery(string query, SqlParameter[] sqlParameters)
+        {
+            using(SqlConnection conn = new SqlConnection(connString))
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddRange(sqlParameters);
+                try
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }catch(SqlException e)
+                {
+                    Console.WriteLine("Error ExecuteInsertUpdateDeleteQuery - " + e.Message);
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
