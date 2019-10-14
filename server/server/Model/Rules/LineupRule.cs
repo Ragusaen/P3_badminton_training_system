5f061d55a5807f2f067bd6693953071505095178
kasper skaarup <kskskaarup@gmail.com>
2019-10-14 08:51:57 +0200

    Rules

--- server/server/Model/Rules/LineupRule.cs
+++ server/server/Model/Rules/LineupRule.cs
@@ -18,44 +18,51 @@ namespace server.Model.Rules
 
             return RuleBreaks;
         }
-        public void LineupSingleCheck(List<IPosition> list)
+        public void LineupSingleCheck(List<IPosition> list, List<IPosition> list2, Leagues league)
         {
 
             foreach (IPosition position in list)
             {
-                foreach()
+                foreach (IPosition position2 in list2) 
+                {
+                    SingleCheck(position.Player.First(), position2.Player.First(), league);
+                }
             }
         }
 
-        public void LineupDoubleCheck(List<IPosition> list)
+        public void LineupDoubleCheck(List<IPosition> list, List<IPosition> list2, Leagues league)
         {
             foreach (IPosition position in list)
             {
-                for (int i = list.IndexOf(position) + 1; i < list.Count; i++)
-                    DoubleCheck(position.Player.First(), position.Player.Last(), list[i].Player.First(), list[i].Player.Last());
+                foreach (IPosition position2 in list2)
+                {
+                    DoubleCheck(position.Player.First(), position.Player.Last(), position2.Player.First(), position2.Player.Last(), league);
+                }
             }
         }
-        public void LineupMixCheck(List<IPosition> list)
+        public void LineupMixCheck(List<IPosition> list, List<IPosition> list2, Leagues league)
         {
             foreach (IPosition position in list)
             {
-                for (int i = list.IndexOf(position) + 1; i < list.Count; i++)
-                    MixCheck(position.Player.First(), position.Player.Last(), list[i].Player.First(), list[i].Player.Last());
+                foreach (IPosition position2 in list2)
+                {
+                    MixCheck(position.Player.First(), position.Player.Last(), position2.Player.First(), position2.Player.Last(), league);
+                }
             }
         }
-        public void SingleCheck(Player UpperPlayer, Player LowerPlayer, Lineup lineup)
+        public void SingleCheck(Player UpperPlayer, Player LowerPlayer, Leagues league)
         {
-            if (lineup.League >= Leagues.Devision1 || lineup.League < Leagues.DenmarkSeries && UpperPlayer.RankingSingle.Points < (LowerPlayer.RankingSingle.Points - 50) ||
-                lineup.League < Leagues.Devision1 || lineup.League >= Leagues.DenmarkSeries && UpperPlayer.RankingLevel.Points < (LowerPlayer.RankingLevel.Points - 50))
+            if (league >= Leagues.Devision1 || league < Leagues.DenmarkSeries && UpperPlayer.RankingSingle.Points < (LowerPlayer.RankingSingle.Points - 50) ||
+                league < Leagues.Devision1 || league >= Leagues.DenmarkSeries && UpperPlayer.RankingLevel.Points < (LowerPlayer.RankingLevel.Points - 50))
             {
                 RuleBreaks.Add(new RuleBreak(UpperPlayer, "Lower player has to many points"));
                 RuleBreaks.Add(new RuleBreak(LowerPlayer, "Upper player has to few points"));
             }
         }
-        public void DoubleCheck(Player UpperPlayer1, Player UpperPlayer2, Player LowerPlayer1, Player LowerPlayer2, Lineup lineup)
+        public void DoubleCheck(Player UpperPlayer1, Player UpperPlayer2, Player LowerPlayer1, Player LowerPlayer2, Leagues league)
         {
-            if (lineup.League >= Leagues.Devision1 || lineup.League < Leagues.DenmarkSeries && UpperPlayer1.RankingDouble.Points + UpperPlayer2.RankingDouble.Points < (LowerPlayer1.RankingDouble.Points + LowerPlayer2.RankingDouble.Points - 100) ||
-                lineup.League < Leagues.Devision1 || lineup.League >= Leagues.DenmarkSeries && UpperPlayer1.RankingLevel.Points + UpperPlayer2.RankingLevel.Points < (LowerPlayer1.RankingLevel.Points + LowerPlayer2.RankingLevel.Points - 100))
+            if (league >= Leagues.Devision1 || league < Leagues.DenmarkSeries && UpperPlayer1.RankingDouble.Points + UpperPlayer2.RankingDouble.Points < (LowerPlayer1.RankingDouble.Points + LowerPlayer2.RankingDouble.Points - 100) ||
+                league < Leagues.Devision1 || league >= Leagues.DenmarkSeries && UpperPlayer1.RankingLevel.Points + UpperPlayer2.RankingLevel.Points < (LowerPlayer1.RankingLevel.Points + LowerPlayer2.RankingLevel.Points - 100))
             {
                 RuleBreaks.Add(new RuleBreak(UpperPlayer1, "Lower player has to many points"));
                 RuleBreaks.Add(new RuleBreak(UpperPlayer2, "Lower player has to many points"));
@@ -63,10 +70,10 @@ namespace server.Model.Rules
                 RuleBreaks.Add(new RuleBreak(LowerPlayer2, "Upper player has to few points"));
             }
         }
-        public void MixCheck(Player UpperPlayer1, Player UpperPlayer2, Player LowerPlayer1, Player LowerPlayer2, Lineup lineup)
+        public void MixCheck(Player UpperPlayer1, Player UpperPlayer2, Player LowerPlayer1, Player LowerPlayer2, Leagues league)
         {
-            if (lineup.League >= Leagues.Devision1 || lineup.League < Leagues.DenmarkSeries && UpperPlayer1.RankingMixed.Points + UpperPlayer2.RankingMixed.Points < (LowerPlayer1.RankingMixed.Points + LowerPlayer2.RankingMixed.Points - 100) ||
-                lineup.League < Leagues.Devision1 || lineup.League >= Leagues.DenmarkSeries && UpperPlayer1.RankingLevel.Points + UpperPlayer2.RankingLevel.Points < (LowerPlayer1.RankingLevel.Points + LowerPlayer2.RankingLevel.Points - 100))
+            if (league >= Leagues.Devision1 || league < Leagues.DenmarkSeries && UpperPlayer1.RankingMixed.Points + UpperPlayer2.RankingMixed.Points < (LowerPlayer1.RankingMixed.Points + LowerPlayer2.RankingMixed.Points - 100) ||
+                league < Leagues.Devision1 || league >= Leagues.DenmarkSeries && UpperPlayer1.RankingLevel.Points + UpperPlayer2.RankingLevel.Points < (LowerPlayer1.RankingLevel.Points + LowerPlayer2.RankingLevel.Points - 100))
             {
                 RuleBreaks.Add(new RuleBreak(UpperPlayer1, "Lower player has to many points"));
                 RuleBreaks.Add(new RuleBreak(UpperPlayer2, "Lower player has to many points"));
