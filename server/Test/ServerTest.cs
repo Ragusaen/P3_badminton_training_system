e9345addbe81ed5584306ef26e3c212a661b0a1a
Ragusaen <rtollu18@student.aau.dk>
2019-10-15 14:28:56 +0200

    Cleanup

--- server/Test/ServerTest.cs
+++ server/Test/ServerTest.cs
@@ -1,30 +1,21 @@
-﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using Server.Controller.Network;
-using System.Threading;
-using Server.Controller.Requests.Serialization;
-using Server.Controller.Requests;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-using System.Net;
-using System.Net.Sockets;
-using System.Net.Security;
-using System.Security.Authentication;
-using System.Security.Cryptography.X509Certificates;
+using Server.Controller.Network;
+using Server.Controller.Requests;
+using System.Threading;
 
 namespace Test
 {
-    [TestFixture]
-    [Parallelizable]
-    class ServerTest
+    [TestClass]
+    public class ServerTest
     {
-        [Test]
-        public void connection_test()
+        [TestMethod]
+        public void ConnectionTest()
         {
             byte[] expected = new byte[] { 1 };
-            
-            SslTcpServer server = new SslTcpServer(TestContext.CurrentContext.TestDirectory + "\\localhost.cer");
+
+            SslTcpServer server = new SslTcpServer(AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "\\localhost.cer");
             SslTcpClient client = new SslTcpClient();
 
             Thread serverThread = new Thread(new ThreadStart(server.RunServer));
@@ -38,6 +29,5 @@ namespace Test
 
             CollectionAssert.AreEqual(expected, actual);
         }
-
     }
 }
