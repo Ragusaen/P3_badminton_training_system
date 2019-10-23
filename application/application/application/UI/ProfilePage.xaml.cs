6305df3fd38ffe49a03c0c5e62d7c4fbe19dfa87
Pernille Pedersen <pped10@student.aau.dk>
2019-10-23 21:35:52 +0200

    ProfilePage+billeder

---
+++ application/application/application/UI/ProfilePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using application.ViewModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace application.UI
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProfilePage : ContentPage
+    {
+        public ProfilePage()
+        {
+            InitializeComponent();
+            ProfilePageViewModel vm = new ProfilePageViewModel();
+            BindingContext = vm;
+            vm.Navigation = Navigation;
+
+            MenuItem1.IconImageSource = ImageSource.FromResource("application.Images.menuicon.jpg");
+            MenuItem2.IconImageSource = ImageSource.FromResource("application.Images.plusicon.jpg");
+        }
+    }
+}
---
+++ application/application/application/ViewModel/ProfilePageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using application.Model;
+
+namespace application.ViewModel
+{
+    class ProfilePageViewModel : BaseViewModel
+    {
+        public Member CurrentMember { get; set; } = new Member();
+        
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                SetProperty(ref _name, value);
+            }
+        }
+        public ProfilePageViewModel() 
+        {
+            CurrentMember.Name = "Pernille Pedersen";
+            Name = CurrentMember.Name;
+        }
+    }
+}
