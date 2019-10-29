a8990fdcaddaeb8df959125050f36a87568dfc54
Pernille Pedersen <pped10@student.aau.dk>
2019-10-29 10:24:09 +0100

    ProfilePage

---
+++ application/application/application/Model/PracticeTeam.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace application.Model
+{
+    class PracticeTeam
+    {
+        public string Name { get; set; }
+
+        public bool OnTeam { get; set; }
+
+        public PracticeTeam(string name, bool onTeam)
+        {
+            Name = name;
+            OnTeam = onTeam;
+        }
+    }
+}
--- application/application/application/ViewModel/ProfilePageViewModel.cs
+++ application/application/application/ViewModel/ProfilePageViewModel.cs
@@ -19,10 +19,41 @@ namespace application.ViewModel
                 SetProperty(ref _name, value);
             }
         }
+        private List<PracticeTeam> _teams;
+
+        public List<PracticeTeam> Teams
+        {
+            get { return _teams; }
+            set
+            {
+                SetProperty(ref _teams, value);
+            }
+        }
+
+
         public ProfilePageViewModel() 
         {
+            Teams = new List<PracticeTeam>();
             CurrentMember.Name = "Pernille Pedersen";
             Name = CurrentMember.Name;
+            Teams.Add(new PracticeTeam("U17", true));
+            Teams.Add(new PracticeTeam("Senior", false));
+
+        }
+        private RelayCommand _teamToggleCommand;
+
+        public RelayCommand TeamToggleCommand
+        {
+            get
+            {
+                return _teamToggleCommand ?? (_teamToggleCommand = new RelayCommand(param => ExecuteTeamToogle(param)));
+            }
+        }
+
+        //Check if user is in database. Navigate to main page.
+        private void ExecuteTeamToogle(object param)
+        {
+
         }
     }
 }
