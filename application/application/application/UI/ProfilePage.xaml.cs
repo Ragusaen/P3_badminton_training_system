3181c5a39a8b29789c49390e85e20957014a1d5a
Pernille Pedersen <pped10@student.aau.dk>
2019-11-01 15:51:38 +0100

    Graph

--- application/application/application/UI/ProfilePage.xaml.cs
+++ application/application/application/UI/ProfilePage.xaml.cs
@@ -4,26 +4,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using application.ViewModel;
+using Microcharts;
+using SkiaSharp;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Entry = Microcharts.Entry;
 
 namespace application.UI
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ProfilePage : ContentPage
     {
+        List<Entry> entries = new List<Entry>
+        {
+            new Entry (5)
+            {
+                Color = SKColor.Parse("#33ccff"),
+                Label = "Dato",
+                ValueLabel = "5"
+
+            },
+
+            new Entry(2)
+            {
+                Color = SKColor.Parse("#ff3399"),
+                Label = "Dato",
+                ValueLabel = "2"
+            },
+
+             new Entry(3)
+            {
+                Color = SKColor.Parse("#0099ff"),
+                Label = "Dato",
+                
+               
+                ValueLabel = "3"
+            }
+        };
+
         public ProfilePage()
         {
             InitializeComponent();
+
+            Chart1.Chart = new LineChart { Entries = entries };
+
             
             ProfilePageViewModel vm = new ProfilePageViewModel();
             BindingContext = vm;
             vm.Navigation = Navigation;
 
- 
 
             Menuicon.Source = ImageSource.FromResource("application.Images.menuicon.jpg");
             Settingsicon.Source = ImageSource.FromResource("application.Images.settingsicon.jpg");
+
         }
     }
 }
