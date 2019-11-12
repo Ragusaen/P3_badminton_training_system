5f33327034d0ac5f2f32918d7e56475eaa3ff982
Ragusaen <rtollu18@student.aau.dk>
2019-11-12 11:56:14 +0100

    jsdalfkj

---
+++ server/server/DAL/PlaysessionCast.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Model;
+
+namespace Server.DAL
+{
+    partial class playsession
+    {
+        public static explicit operator Common.Model.PlaySession(playsession p)
+        {
+            PlaySession playSession;
+            if (p.Type == (int)PlaySession.Type.Practice)
+            {
+                var practice = p.practicesession;
+                playSession = new PracticeSession()
+                {
+                    PracticeTeam = (Common.Model.PracticeTeam)practice.yearplansection.practiceteam;
+                };
+            }
+            else
+            {
+                var match = p.teammatch;
+                playSession = new Match()
+                {
+
+                };
+            }
+
+            playSession.End = p.EndDate;
+            playSession.Start = p.StartDate;
+            playSession.Location = p.Location;
+            playSession.Id = p.ID; 
+
+            return 
+        }
+    }
+}
