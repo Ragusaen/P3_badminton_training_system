69033f2c3b8ea3bdd50f29c184e67ac1fda4b847
KristianOdum <kristianodum@gmail.com>
2019-11-12 09:51:42 +0100

    Update GetPracticeTeam.cs

--- Common/Serialization/GetPracticeTeam.cs
+++ Common/Serialization/GetPracticeTeam.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.Serialization;
 using System.Text;
+using Common.Model;
 
 namespace Common.Serialization
 {
-    class GetPracticeTeam
+    [DataContract]
+    public class GetPracticeTeamRequest
     {
+        [DataMember] public int Id;
+    }
+
+    [DataContract]
+    public class GetPracticeTeamResponse
+    {
+        [DataMember] public PracticeTeam Team;
     }
 }
