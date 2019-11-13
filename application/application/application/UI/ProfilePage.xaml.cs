45565ca2d623a8b80a0bd11eade5ec7f2e84ef1b
Slorup <nicklas.slorup@yahoo.dk>
2019-11-13 09:02:45 +0100

    Fix swipe + nav problems

--- application/application/application/UI/ProfilePage.xaml.cs
+++ application/application/application/UI/ProfilePage.xaml.cs
@@ -8,28 +8,27 @@ using Microcharts;
 using SkiaSharp;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using Entry = Microcharts.Entry;
 
 namespace application.UI
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ProfilePage : ContentPage
     {
-        List<Entry> entries = new List<Entry>
+        List<Microcharts.Entry> entries = new List<Microcharts.Entry>
         {
-            new Entry (2)
+            new Microcharts.Entry (2)
             {
                 Color = SKColor.Parse("#33ccff"),
                 Label = "Dato"
             },
 
-            new Entry(-1)
+            new Microcharts.Entry(-1)
             {
                 Color = SKColor.Parse("#ff3399"),
                 Label = "Dato"
             },
 
-             new Entry(0)
+             new Microcharts.Entry(0)
             {
                 Color = SKColor.Parse("#0099ff"),
                 Label = "Dato"
@@ -41,14 +40,10 @@ namespace application.UI
             InitializeComponent();
 
             Chart1.Chart = new LineChart { Entries = entries, LineMode = LineMode.Straight, PointMode = PointMode.Square, LabelTextSize = 25, PointSize = 12};
-
             
             ProfilePageViewModel vm = new ProfilePageViewModel();
             BindingContext = vm;
             vm.Navigation = Navigation;
-
-
-            Settingsicon.Source = ImageSource.FromResource("application.Images.settingsicon.jpg");
 
             Settingsicon.Source = ImageSource.FromResource("application.Images.settingsicon.jpg");
         }
