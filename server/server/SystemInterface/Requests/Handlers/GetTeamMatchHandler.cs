745448ecf9fb692641f9bcf961d35ea8ddccddd5
Ragusaen <rtollu18@student.aau.dk>
2019-11-18 12:38:50 +0100

    kjasdfjæl

---
+++ server/server/SystemInterface/Requests/Handlers/GetTeamMatchHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Serialization;
+using Server.DAL;
+
+namespace Server.SystemInterface.Requests.Handlers
+{
+    class GetTeamMatchHandler : MiddleRequestHandler<GetTeamMatchRequest, GetTeamMatchResponse>
+    {
+        protected override GetTeamMatchResponse InnerHandle(GetTeamMatchRequest request, member requester)
+        {
+            var db = new DatabaseEntities();
+            return new GetTeamMatchResponse
+            {
+                TeamMatches = (Common.Model.PlaySession) db.playsessions.Single(p => p.ID == request.PlaySessionId)
+            };
+        }
+    }
+}
