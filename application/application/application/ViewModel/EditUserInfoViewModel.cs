21d8e1b03881d4d284cd19b42d5402afcab62da7
kasper skaarup <kskskaarup@gmail.com>
2019-11-19 10:18:01 +0100

    Member

--- application/application/application/ViewModel/EditUserInfoViewModel.cs
+++ application/application/application/ViewModel/EditUserInfoViewModel.cs
@@ -7,9 +7,10 @@ namespace application.ViewModel
 {
     class EditUserInfoViewModel : BaseViewModel
     {
+        public Member Member { get; set; }
         public EditUserInfoViewModel(Member member)
         {
-
+            Member = member;
         }
     }
 }
