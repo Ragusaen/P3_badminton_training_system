694d224a8b669ee4626be21355ad6f3ff39aff91
Ragusaen <rtollu18@student.aau.dk>
2019-11-21 13:20:42 +0100

    klajdkja

---
+++ server/server/SystemInterface/Requests/Handlers/DeletePlayerPracticeTeamHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Model;
+using Common.Serialization;
+using NLog;
+using Server.DAL;
+
+namespace Server.SystemInterface.Requests.Handlers
+{
+    class DeletePlayerPracticeTeamHandler : MiddleRequestHandler<DeletePlayerPracticeTeamRequest, DeletePlayerPracticeTeamResponse>
+    {
+        private static Logger _log = LogManager.GetCurrentClassLogger();
+        protected override DeletePlayerPracticeTeamResponse InnerHandle(DeletePlayerPracticeTeamRequest request, member requester)
+        {
+            if (!(requester.MemberType == (int) MemberType.Trainer || request.Player.Member.Id == requester.ID))
+                return null;
+
+            var db = new DatabaseEntities();
+            var dbPt = db.practiceteams.Find(request.PracticeTeam.Id);
+            db.members.Find(request.Player.Member.Id).practiceteams.Remove(dbPt);
+
+            _log.Debug($"Player: {request.Player.Member.Name} removed from Practice Team: {dbPt.Name}");
+
+            return new DeletePlayerPracticeTeamResponse();
+        }
+    }
+}
---
+++ server/server/SystemInterface/Requests/Handlers/GetAllPracticeTeamsHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Model;
+using Common.Serialization;
+using NLog;
+using Server.DAL;
+
+namespace Server.SystemInterface.Requests.Handlers
+{
+    class GetAllPracticeTeamsHandler : MiddleRequestHandler<GetAllPracticeTeamsRequest, GetAllPracticeTeamsResponse>
+    {
+        private static Logger _log = LogManager.GetCurrentClassLogger();
+        protected override GetAllPracticeTeamsResponse InnerHandle(GetAllPracticeTeamsRequest request, member requester)
+        {
+            var db = new DatabaseEntities();
+
+            var result = db.practiceteams.ToList().Select(p => (Common.Model.PracticeTeam) p).ToList();
+            _log.Debug($"Fetching all practice teams, count = {result.Count}");
+            return new GetAllPracticeTeamsResponse
+            {
+                PracticeTeams = result
+            };
+        }
+    }
+}
