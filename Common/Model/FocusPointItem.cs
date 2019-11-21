760aca058fa73f2b85a28f1309407e926eab2619
kasper skaarup <kskskaarup@gmail.com>
2019-11-21 08:51:19 +0100

    okay

--- Common/Model/FocusPointItem.cs
+++ Common/Model/FocusPointItem.cs
@@ -5,7 +5,6 @@ namespace Common.Model
     public class FocusPointItem
     {
         public DateTime DateAssigned { get; set; }
-
         public FocusPointDescriptor Descriptor { get; set; }
     }
 }
