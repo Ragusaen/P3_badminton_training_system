aa5283dc0fbecf3aa0d54db0510c3f0028b97554
Ragusaen <rtollu18@student.aau.dk>
2019-11-25 12:51:40 +0100

    lkæajdkj

--- Common/Model/Feedback.cs
+++ Common/Model/Feedback.cs
@@ -6,11 +6,11 @@ namespace Common.Model
     public class Feedback
     {
         public Player Player { get; set; }
-        public PracticeSession PlaySession { get; set; }
-        public int? ReadyQuestion { get; set; }
-        public int? EffortQuestion { get; set; }
-        public int? ChallengeQuestion { get; set; }
-        public int? AbsorbQuestion { get; set; }
+        public PlaySession PlaySession { get; set; }
+        public int ReadyQuestion { get; set; }
+        public int EffortQuestion { get; set; }
+        public int ChallengeQuestion { get; set; }
+        public int AbsorbQuestion { get; set; }
         public string GoodQuestion { get; set; }
         public string BadQuestion { get; set; }
         public string FocusPointQuestion { get; set; }
