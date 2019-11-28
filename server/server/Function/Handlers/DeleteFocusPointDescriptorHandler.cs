e347b73c3741fed6895fa1933cf8442b6b3894f0
KristianOdum <kristianodum@gmail.com>
2019-11-28 16:07:47 +0100

    Nusning

--- server/server/Function/Handlers/DeleteFocusPointDescriptorHandler.cs
+++ server/server/Function/Handlers/DeleteFocusPointDescriptorHandler.cs
@@ -17,10 +17,24 @@ namespace Server.SystemInterface.Requests.Handlers
             {
                 return new DeleteFocusPointDescriptorResponse { AccessDenied = true };
             }
+
             var db = new DatabaseEntities();
             var fp = db.focuspoints.Find(request.FocusPointDescriptor.Id);
             if (fp != null)
             {
+                var players = fp.members;
+                var pracsMain = fp.practicesessionsmain;
+                var pracsSub = fp.practicesessionssub;
+
+                foreach (var p in players)
+                    p.focuspoints.Remove(fp);
+
+                foreach (var prac in pracsMain)
+                    prac.mainfocuspoint = null;
+
+                foreach (var prac in pracsSub)
+                    prac.subfocuspoints.Remove(fp);
+
                 db.focuspoints.Remove(fp);
                 _log.Debug($"Completely removed focus point descriptor {fp.Name}");
             }
