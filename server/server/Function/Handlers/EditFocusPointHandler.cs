144df9f7f20648685ed595af4e2c3a65c0490830
Ragusaen <rtollu18@student.aau.dk>
2019-11-28 13:58:21 +0100

    kljd

---
+++ server/server/Function/Handlers/EditFocusPointHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Serialization;
+using Server.DAL;
+using Server.SystemInterface.Requests.Handlers;
+
+namespace Server.Function.Handlers
+{
+    class EditFocusPointHandler : MiddleRequestHandler<EditFocusPointRequest, EditFocusPointResponse>
+    {
+        protected override EditFocusPointResponse InnerHandle(EditFocusPointRequest request, member requester)
+        {
+            var db = new DatabaseEntities();
+            var dbFp = db.focuspoints.Find(request.FP.Id);
+            
+            dbFp.Name = request.FP.Name;
+            dbFp.Description = request.FP.Description;
+            dbFp.VideoURL = request.FP.VideoURL;
+
+            db.SaveChanges();
+            return new EditFocusPointResponse();
+        }
+    }
+}
